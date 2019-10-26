cc23540433e5e5885dec51eb1aaac645d1c39196
769217 <769217@ELF8IT221600.ACDM.DS.SAIT.CA>
2019-10-26 17:11:00 -0600

    .

--- src/Feedback.java
+++ src/Feedback.java
@@ -1,21 +1,28 @@
 
 /**
- * 
+ * Contains the attributes and methods for a Feedback object.
  * 
  * @author Viktoriia Davydova (769217)
  *
  */
 public class Feedback {
 
-/**
- * 
- */
+	/**
+	 * The Customer object under which the feedback will be sent. 
+	 */
 	private Customer customer;
-	
+
+	/**
+	 * The message which the Feedback object will contain.Cannot be null or
+	 * contain more than 300 characters
+	 */
 	private String message;
-	
+
+	/**
+	 * The method to submit the feedback
+	 */
 	public void submitFeedback() {
-		
+
 	}
 
 }
