be7ace5ea849aaf956b942448c2b7156f76493bd
Holly <hharnack@hotmail.com>
2019-10-26 16:42:35 -0600

    user

--- src/User.java
+++ src/User.java
@@ -1,10 +1,26 @@
 /**
- * Class Description:  
+ * Class Description: This class represents the user using the system
  * 
  * @author 806547 Holly Harnack
  */
-public class User {
+public abstract class User {
 
-	
-	
+	/**
+	 * The id number of the 
+	 */
+	private int idNumber;
+	private String firstName;
+	private String lastName;
+	private String username;
+	private String password;
+	private String email;
+	private Address address;
+
+	/**
+	 * Encrypts the entered password and sets the password for the user to the new password.
+	 */
+	public void changePassword() {
+
+	}
+
 }
