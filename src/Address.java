4c1069cc6dfa1c31a78b7f3edbf0440e4c2e52fc
Holly <hharnack@hotmail.com>
2019-10-26 17:34:00 -0600

    changing address

--- src/Address.java
+++ src/Address.java
@@ -1,4 +1,36 @@
-
+/**
+ * class description: Class for storing address information
+ *
+ * @author Carsen Johns (640195)
+ *
+ */
 public class Address {
+	/**
+	 * street information
+	 */
+	private String street;
+	/**
+	 * postal code information
+	 */
+	private String postal;
+	/**
+	 * city information
+	 */
+	private String city;
+	/**
+	 * province information
+	 */
+	private String province;
+	/**
+	 * country information
+	 */
+	private String country;
 
+	/**
+	 * Return a concatenated string of each address component
+	 * 
+	 */
+	public String getAddress() {
+		return "";
+	};
 }
