a4afa1958c362ce1a7ff85f24bbadd7b055fe583
769217 <769217@ELF8IT221600.ACDM.DS.SAIT.CA>
2019-10-26 17:29:17 -0600

    .

--- src/FeedbackManager.java
+++ src/FeedbackManager.java
@@ -2,6 +2,7 @@ import java.util.List;
 
 /**
  * The class to manipulate the list of the Feedback objects
+ * 
  * @author Viktoriia Davydova (769217)
  *
  */
@@ -11,28 +12,32 @@ public class FeedbackManager {
 	 * The list to contain the Feedback objects
 	 */
 	private List<Feedback> feedbackList;
-	
+
 	/**
 	 * Initializes the newly created FeedbackManager
 	 */
 	public FeedbackManager() {
-		
+
 	}
+
 	/**
 	 * The method to view the Feedback before delete it or post on the website
 	 */
 	private void viewFeedback() {
-		
+
 	}
-	
+
 	/**
-	 * 
+	 * The method to delete the Fedback
 	 */
 	private void deleteFeedback() {
-		
+
 	}
-	
+
+	/**
+	 * The method to post the Feedback to the website
+	 */
 	private void postToWebsite() {
-		
+
 	}
 }
--- src/UserManager.java
+++ src/UserManager.java
@@ -1,7 +1,8 @@
 import java.util.List;
 
 /**
- * Contains methods to manipulate the list of User objects. 
+ * Contains methods to manipulate the list of User objects.
+ * 
  * @author Viktoriia Davydova (769217)
  *
  */
@@ -11,47 +12,48 @@ public class UserManager {
 	 * The list of all the users of the system.
 	 */
 	private List userList;
-	
+
 	/**
 	 * Initializes the newly created UserManager
 	 */
 	public UserManager() {
-		
+
 	}
-	
+
 	/**
 	 * The method to create the new User object in the system
 	 */
 	public void createUserAccount() {
-		
+
 	}
-	
+
 	/**
-	 * The method to look through the list of the User objects 
+	 * The method to look through the list of the User objects
 	 */
 	public void viewUsers() {
-		
+
 	}
-	
+
 	/**
 	 * The method to edit the customer User information
 	 */
 	public void editCustomer() {
-		
+
 	}
-	
+
 	/**
-	 * The method to disable the User object to prevent the user from using the system
+	 * The method to disable the User object to prevent the user from using the
+	 * system
 	 */
 	public void disableUser() {
-		
+
 	}
-	
+
 	/**
 	 * The method to edit the user information either administrator or customer
 	 */
 	public void editUser() {
-		
+
 	}
 
 }
