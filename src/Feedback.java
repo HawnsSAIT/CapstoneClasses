a6a69850344db8278b9984026d14416b462e68c5
769217 <769217@ELF8IT221600.ACDM.DS.SAIT.CA>
2019-10-26 17:21:51 -0600

    .

--- src/Feedback.java
+++ src/Feedback.java
@@ -1,24 +1,32 @@
 
 /**
  * Contains the attributes and methods for a Feedback object.
+ * 
  * @author Viktoriia Davydova (769217)
  *
  */
 public class Feedback {
 
 	/**
-	 * The Customer object under which the feedback will be sent. 
+	 * The Customer object under which the feedback will be sent.
 	 */
 	private Customer customer;
 
 	/**
-	 * The message which the Feedback object will contain.Cannot be null or
-	 * contain more than 300 characters
+	 * The message which the Feedback object will contain.Cannot be null or contain
+	 * more than 300 characters
 	 */
 	private String message;
 
 	/**
-	 * The method to submit the feedback
+	 * Initializes the newly created Feedback object
+	 */
+	public Feedback() {
+
+	}
+
+	/**
+	 * The method to submit the feedback to the website
 	 */
 	public void submitFeedback() {
 
--- src/FeedbackManager.java
+++ src/FeedbackManager.java
@@ -1,4 +1,38 @@
+import java.util.List;
 
+/**
+ * The class to manipulate the list of the Feedback objects
+ * @author Viktoriia Davydova (769217)
+ *
+ */
 public class FeedbackManager {
 
+	/**
+	 * The list to contain the Feedback objects
+	 */
+	private List<Feedback> feedbackList;
+	
+	/**
+	 * Initializes the newly created FeedbackManager
+	 */
+	public FeedbackManager() {
+		
+	}
+	/**
+	 * The method to view the Feedback before delete it or post on the website
+	 */
+	private void viewFeedback() {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	private void deleteFeedback() {
+		
+	}
+	
+	private void postToWebsite() {
+		
+	}
 }
