6af81806c01701eda783b080100d753646830a81
553185 <553185@ELF8IT221603.ACDM.DS.SAIT.CA>
2019-10-26 17:15:34 -0600

    doc

--- src/ReportManager.java
+++ src/ReportManager.java
@@ -1,4 +1,43 @@
+import java.util.ArrayList;
 
+/**
+ * Class for carrying out any tasks associated with reports.
+ * @author Levon Rose (553185)
+ * 
+ */
 public class ReportManager {
-
+	
+	/**
+	 * A list of the results taken from the database.
+	 */
+	private ArrayList resultSet;
+	
+	/**
+	 * Method for getting parameters to be used in the report.
+	 */
+	public void getParameters() {
+		
+	};
+	
+	/**
+	 * Method for getting information from the database.
+	 */
+	public void queryDatabase() {
+		
+	};
+	
+	/**
+	 * Method for formatting reports.
+	 */
+	public void formatReport() {
+		
+	};
+	
+	/**
+	 * Method for printing out reports.
+	 */
+	public void printReport() {
+		
+	};
+	
 }
