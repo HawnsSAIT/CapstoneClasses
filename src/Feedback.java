9fe8e778ef943e51824ed785f6d07ee948693619
769217 <769217@ELF8IT221600.ACDM.DS.SAIT.CA>
2019-10-26 17:04:31 -0600

    .

--- src/Feedback.java
+++ src/Feedback.java
@@ -1,10 +1,21 @@
 
+/**
+ * 
+ * 
+ * @author Viktoriia Davydova (769217)
+ *
+ */
 public class Feedback {
-	
-//	-customer:Customer
-//	-message
-//	+submitFeedback()
 
+/**
+ * 
+ */
 	private Customer customer;
+	
+	private String message;
+	
+	public void submitFeedback() {
+		
+	}
 
 }
