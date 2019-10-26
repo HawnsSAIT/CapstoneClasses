827775429fc3f7c334ddd12c5d4e2c91d257175d
Hawns4 <44388744+Hawns4@users.noreply.github.com>
2019-10-26 15:48:12 -0600

    Pet attributes and javadog

---
+++ src/Pet.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Pet {
+	
+	/**
+	 * Dog's ID number to identify it in the database. Cannot be null or negative.
+	 */
+	private String idNumber;
+	
+	/**
+	 * Dog's name. Cannot be null or contain non-alphabetical characters.
+	 */
+	private String name;
+	
+	/**
+	 * Dog's date of birth. Cannot be null or past current date.
+	 */
+	private Date dateOfBirth;
+	
+	/**
+	 * Dog's breed. Cannot be null.
+	 */
+	private String breed;
+	
+	/**
+	 * If the dog has been spayed or neutered.
+	 */
+	private boolean spayedNeutered;
+	
+	/**
+	 * Name of veterinarian.
+	 */
+	private String veterinarian;
+	
+	/**
+	 * List of dog's medications.
+	 */
+	private ArrayList<String> medications;
+	
+	/**
+	 * List of dog's allergies
+	 */
+	private ArrayList<String> allergies;
+	
+	/**
+	 * List of dog's vaccines.
+	 */
+	private ArrayList<String> vaccines;
+	
+	/**
+	 * If the dog is comfortable around strangers.
+	 */
+	private boolean strangerComfortable;
+	
+	/**
+	 * If the dog is friendly towards other large dogs.
+	 */
+	private boolean largeDogFriendly;
+	
+	/**
+	 * If the dog is friendly towards other small dogs.
+	 */
+	private boolean smallDogFriendly;
+	
+	/**
+	 * If the dog is friendly towards newborn dogs.
+	 */
+	private boolean puppyFriendly;
+	
+}
