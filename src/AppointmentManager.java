3e0ce8b914b7f7433bb78db67430399aa8bdd2b4
769217 <769217@ELF8IT221600.ACDM.DS.SAIT.CA>
2019-10-26 16:38:28 -0600

    .

---
+++ src/AppointmentManager.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+/**
+ * 
+ * Class description:
+ *
+ * @author Viktoriia Davydova (769217)
+ *
+ */
+public class AppointmentManager {
+
+//	-appointmentList:List
+//	+bookNewAppointment()
+//	+editAppointment()
+//	+cancelAppointment()
+//	+displayAppointments()
+	
+	private List appointmentList;
+	
+}
