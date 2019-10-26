ce733ae7e8ef023a962d1b01e74ddefb5ddb4ef2
769217 <769217@ELF8IT221600.ACDM.DS.SAIT.CA>
2019-10-26 16:06:05 -0600

    working on it

--- src/UserManager.java
+++ src/UserManager.java
@@ -1,4 +1,37 @@
+import java.util.List;
 
+/**
+ * 
+ * Class description:
+ *
+ * 
+ *
+ */
 public class UserManager {
+
+	/**
+	 * The list of all the users of the system. 
+	 */
+	private List userList;
+	
+	public void createUserAccount() {
+		
+	}
+	
+	public void viewUsers() {
+		
+	}
+	
+	public void editCustomer() {
+		
+	}
+	
+	public void disableUser() {
+		
+	}
+	
+	public void editUser() {
+		
+	}
 
 }
