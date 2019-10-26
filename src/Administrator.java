ff2f01b423462656e1ed0b535541f3017d1f5f34
553185 <553185@ELF8IT221603.ACDM.DS.SAIT.CA>
2019-10-26 16:26:50 -0600

    doc

--- src/Administrator.java
+++ src/Administrator.java
@@ -1,4 +1,23 @@
 
+/**
+ * Class used to model an administrator.
+ * @author Levon Rose (553185)
+ * 
+ */
 public class Administrator {
-
+	
+	/**
+	 * A userManager object.
+	 */
+	private UserManager userManager;
+	
+	/**
+	 * A reportManager object.
+	 */
+	private ReportManager reportManager;
+	
+	/**
+	 * An acctManager object.
+	 */
+	private AccountManager acctManager;
 }
