a430759fe80d359850ddea54491c8d3422964789
Hans <44388744+Hawns4@users.noreply.github.com>
2019-12-03 17:37:09 -0700

    asdf

--- src/application/logic/managers/AppointmentManager.java
+++ src/application/logic/managers/AppointmentManager.java
@@ -13,33 +13,6 @@ public class AppointmentManager {
 	 * List of existing appointments in the database.
 	 */
 	ArrayList<Appointment> appointmentList;
+
 	
-	/**
-	 * Creates a new appointment object. Cannot book new appointment if start date is earlier than current date.
-	 */
-	public void bookNewAppointment() {
-		
-	}
-	
-	/**
-	 * Edits an existing appointment. Cannot edit an appointment that has past it's end date.
-	 */
-	public void editAppointment() {
-		
-	}
-	
-	/**
-	 * Removes an appointment from the appointmentList. Cannot cancel an appointment that has past it's end date.
-	 */
-	public void cancelAppointment() {
-		
-	}
-	
-	/**
-	 * Gets the appointments that are wanting to be displayed and returns the list.
-	 * @return a list of appointments to be display
-	 */
-	public ArrayList<Appointment> displayAppointments() {
-		return null;
-	}
 }
