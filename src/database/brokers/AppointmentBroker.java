92bb6ffa54d824d2237b72ced6c763a5c565583a
553185 <553185@ELF8IT221603.ACDM.DS.SAIT.CA>
2019-12-04 14:45:08 -0700

    AppointmentBroker doc

--- src/database/brokers/AppointmentBroker.java
+++ src/database/brokers/AppointmentBroker.java
@@ -1,5 +1,83 @@
 package database.brokers;
 
+import java.util.List;
+
+import database.connection.DatabaseConnection;
+import domain.appointments.Appointment;
+
+/**
+ * Broker class for interacting with appointments in the 
+ * database.
+ *
+ * @author Levon Rose (553185)
+ * 
+ */
 public class AppointmentBroker {
 
+	/**
+	 * Database connection used for communicating with database.
+	 */
+	private DatabaseConnection connection;
+	
+	/**
+	 * Method for getting appointment from database.
+	 * 
+	 * @param idNumber - id number used to identify an
+	 * appointment in the database.
+	 * @return dog which was retrieved from database.
+	 */
+	public Appointment get(int idNumber) {
+		return null;
+	}
+	
+	/**
+	 * Method for adding an appointment to the database. 
+	 * 
+	 * @param appointment - appointment to be added to database.
+	 * @return 0 if insert was successful, 1 otherwise.
+	 */
+	public int insert(Appointment appointment) {
+		return 0;
+	}
+	
+	/**
+	 * Method for updating information for appointment in database.
+	 * 
+	 * @return 0 if update was successful, 1 otherwise.
+	 */
+	public int update(Appointment appointment) {
+		return 0;
+	}
+	
+	/**
+	 * Method for deleting appointment from database.
+	 * 
+	 * @param idNumber - id number used to identify an
+	 * appointment in the database.
+	 * @return 0 if delete was successful, 1 otherwise.
+	 */
+	public int delete(int idNumber) {
+		return 0;
+	}
+	
+	/**
+	 * Method for getting list of appointment from database belonging
+	 * to certain user.
+	 * 
+	 * @param username - username of user whose appointments are to be 
+	 * retrieved.
+	 * @return list of appointments that were retrieved from database. 
+	 */
+	public List<Appointment> getUserAppt(String username){
+		return null;
+	}
+	
+	/**
+	 * Method for getting list of all appointments from the database.
+	 * 
+	 * @return list of appointments that were retrieved from database. 
+	 */
+	public List<Appointment> getAll(){
+		return null;
+	}
 }
