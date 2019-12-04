bb32554d86a616e1fadfcfd57585443284644c3d
553185 <553185@ELF8IT221603.ACDM.DS.SAIT.CA>
2019-12-04 13:40:48 -0700

    DataManager doc

--- src/application/logic/managers/DataManager.java
+++ src/application/logic/managers/DataManager.java
@@ -1,20 +1,31 @@
 package application.logic.managers;
 
+/**
+ * Manages data operations.
+ * @author Levon Rose (553185)
+ * 
+ */
 public class DataManager {
 
 	/**
+	 * Method for creating a backup for system information.
 	 * 
-	 * @param filepath
-	 * @return
+	 * @param filepath - String representing the file path where the backup
+	 * will be stored.
+	 * @return true if the backup was created successfully, false otherwise.
 	 */
 	public boolean backup(String filepath) {
 		return false;
 	}
 	
 	/**
+	 * Method for restoring system information from a previously created
+	 * backup file.
 	 * 
-	 * @param restorePoint
-	 * @return
+	 * @param restorePoint - String representing the path where the 
+	 * backup to be restored resides.
+	 * @return true if system information was restored successfully, false
+	 * otherwise.
 	 */
 	public boolean restore(String restorePoint) {
 		return false;
