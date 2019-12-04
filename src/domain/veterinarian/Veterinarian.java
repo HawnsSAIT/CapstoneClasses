457dc27d71bd61269152ebd05dcdfa0e43a25940
Hans <44388744+Hawns4@users.noreply.github.com>
2019-12-04 14:39:22 -0700

    Veterinarian doc

--- src/domain/veterinarian/Veterinarian.java
+++ src/domain/veterinarian/Veterinarian.java
@@ -2,15 +2,26 @@ package domain.veterinarian;
 
 /**
  * Contains information about the Customer's dog's veterinarian.
- * @author Hans
+ * 
+ * @author 703174 Hans Cabrera
  *
  */
 public class Veterinarian {
-	
+
+	/**
+	 * The name of the customer's veterinarian. Cannot be null or contain numbers.
+	 */
 	private String name;
-	
+
+	/**
+	 * The name of the clinic the veterinarian belongs to. Cannot be null.
+	 */
 	private String clinic;
-	
+
+	/**
+	 * The phone number of the veterinarian. Cannot be null, or contain letters, and
+	 * must be 10 digits long.
+	 */
 	private String phoneNumber;
-	
+
 }
