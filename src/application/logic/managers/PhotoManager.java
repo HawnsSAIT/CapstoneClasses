f9dd065f479c1eaac781143bbec243793f947c0c
Hans <44388744+Hawns4@users.noreply.github.com>
2019-12-05 10:53:05 -0700

    asdf

--- src/application/logic/managers/PhotoManager.java
+++ src/application/logic/managers/PhotoManager.java
@@ -1,5 +1,18 @@
 package application.logic.managers;
 
-public class PhotoManager {
+import java.util.List;
 
+import database.brokers.PhotoBroker;
+
+public class PhotoManager {
+	
+	/**
+	 * A list that contains the paths for all of the photos.
+	 */
+	private List<String> photoList;
+	
+	/**
+	 * 
+	 */
+	private PhotoBroker broker;
 }
