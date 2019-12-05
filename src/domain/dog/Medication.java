6a9e0c514664660fe76dca90779e31c89f69d4a8
Holly <hharnack@hotmail.com>
2019-12-05 12:56:54 -0700

    medication

--- src/domain/dog/Medication.java
+++ src/domain/dog/Medication.java
@@ -1,8 +1,20 @@
 package domain.dog;
 
+/**
+ * Medication for the Dog Object.
+ * 
+ * @author 806547 Holly Harnack
+ *
+ */
 public class Medication {
 
+	/**
+	 * The name of the medication. Cannot be null.
+	 */
 	private String medication;
 	
+	/**
+	 * The details of the medication. How and when to administer the medication.
+	 */
 	private String details;
 }
