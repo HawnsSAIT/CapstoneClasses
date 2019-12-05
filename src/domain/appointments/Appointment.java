d2b5ecce891ec93fc96f211895fb61f3fe038cb2
769217 <769217@ELF8IT221600.ACDM.DS.SAIT.CA>
2019-12-05 13:10:59 -0700

    done

--- src/domain/appointments/Appointment.java
+++ src/domain/appointments/Appointment.java
@@ -1,12 +1,13 @@
 package domain.appointments;
+
 import java.util.Date;
 
 import domain.dog.Dog;
 import domain.user.Customer;
 
 /**
- * Superclass to the Training, Boarding, and Grooming subclasses. Appointment
- * contains the attributes of an appointment placed by the user.
+ * Superclass to the Training and Boarding subclasses. Appointment contains the
+ * attributes of an appointment placed by the user.
  * 
  * @author Viktoriia Davydova (769217)
  *
@@ -14,25 +15,54 @@ import domain.user.Customer;
 public abstract class Appointment {
 
 	/**
+	 * ID number of the appointment. Can not be null.
+	 */
+	private int IdNumber;
+
+	/**
+	 * Costumer's username. Can not be null.
+	 */
+	private String username;
+
+	/**
 	 * The start date and time of appointment. Cannot be later than the appointment
 	 * end date and time. Cannot be less than the current date and time.
 	 */
-	private Date aptStartTime;
+	private Date startTime;
 
 	/**
 	 * The end date and time of appointment. Cannot be earlier than the appointment
 	 * start date and time. Cannot be earlier than the current date and time.
 	 */
-	private Date aptEndTime;
+	private Date endTime;
 
 	/**
-	 * The customer who booked the appointment. Cannot be null.
+	 * The total amount paid for the specific appointment.
 	 */
-	private Customer customer;
-	
-	/**
-	 * The pet that is associated with the appointment. Cannot be null.
-	 */
-	private Dog dog; 
+	private double total;
 
+	/**
+	 * The amount paid for the specific appointment.
+	 */
+	private double amountPaid;
+
+	/**
+	 * Is true if the appointment was approved. False otherwise.
+	 */
+	private boolean isApproved;
+
+	/**
+	 * Is true if the appointment was cancelled. False otherwise.
+	 */
+	private boolean isCancelled;
+
+	/**
+	 * Is true if the appointment was paid. False otherwise.
+	 */
+	private boolean isPaid;
+
+	/**
+	 * String to hold additional comments if necessary.
+	 */
+	private String additionalComments;
 }
