7ce845a353176972376071dbd5031c714f99af95
769217 <769217@ELF8IT221600.ACDM.DS.SAIT.CA>
2019-12-05 13:41:55 -0700

    done

--- src/application/logic/managers/TestimonialManager.java
+++ src/application/logic/managers/TestimonialManager.java
@@ -1,11 +1,12 @@
 package application.logic.managers;
+
 import java.util.List;
 
 import database.brokers.TestimonialBroker;
 import domain.testimonial.Testimonial;
 
 /**
- * The class to manipulate the list of the Feedback objects
+ * The class to manipulate the list of the Testimonial objects.
  * 
  * @author Viktoriia Davydova (769217)
  *
@@ -13,45 +14,52 @@ import domain.testimonial.Testimonial;
 public class TestimonialManager {
 
 	/**
-	 * The list to contain the Feedback objects
+	 * List of existing testimonials in the database.
 	 */
-	private List<Testimonial> feedbackList;
+	private List<Testimonial> testimonialList;
 
 	/**
-	 * 
+	 * Broker used for persistence of testimonials.
 	 */
 	private TestimonialBroker broker;
-	
+
 	/**
+	 * Method for adding testimonial to database.
 	 * 
-	 * @param testimony
-	 * @return
+	 * @param testimonial
+	 *            - testimonial to be added to database.
+	 * @return 0 if insert was successful, 1 otherwise.
 	 */
-	public int insert(Testimonial testimony) {
+	public int insert(Testimonial testimonial) {
 		return 0;
 	}
-	
+
 	/**
+	 * Method for updating information for testimonial in database.
 	 * 
 	 * @param idNumber
-	 * @return
+	 *            id number of the testimonial.
+	 * @return 0 if update was successful, 1 otherwise.
 	 */
 	public int update(int idNumber) {
 		return 0;
 	}
-	
+
 	/**
+	 * Method for deleting testimonial from database.
 	 * 
 	 * @param idNumber
-	 * @return
+	 *            - id number used to identify a testimonial in the database.
+	 * @return 0 if delete was successful, 1 otherwise.
 	 */
 	public int delete(int idNumber) {
 		return 0;
 	}
-	
+
 	/**
+	 * Method for getting list of testimonials from database.
 	 * 
-	 * @return
+	 * @return the list of the testimonials that were retrieved from database.
 	 */
 	public List<Testimonial> getAll() {
 		return null;
