07213af34718adc352e9da8e1e0f854aa68b5036
Holly <hharnack@hotmail.com>
2019-12-05 12:45:13 -0700

    allergy

--- src/domain/dog/Allergy.java
+++ src/domain/dog/Allergy.java
@@ -1,8 +1,19 @@
 package domain.dog;
 
+/**
+ * @author 806547
+ *
+ */
 public class Allergy {
 
+	/**
+	 * The name of the allergy. Cannot be null.
+	 */
 	private String allergy;
 	
+	/**
+	 * The details about this allergy and how to monitor it.
+	 */
 	private String details;
+
 }
