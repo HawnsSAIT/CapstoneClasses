34a9e347aceb8fa9c7a79b4b4b746b0d1c0d35a8
Hans <44388744+Hawns4@users.noreply.github.com>
2019-12-05 13:32:42 -0700

    dbc doc

--- src/database/connection/DatabaseConnection.java
+++ src/database/connection/DatabaseConnection.java
@@ -1,5 +1,60 @@
 package database.connection;
 
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+/**
+ * This class contains the singleton pattern used to connect the brokers to the
+ * database.
+ * 
+ * @author 703174 Hans Cabrera
+ *
+ */
 public class DatabaseConnection {
+
+	/**
+	 * A reference of the DatabaseConnection class to enforce.
+	 */
+	private static DatabaseConnection databaseConnection;
+
+	/**
+	 * The database to connect to.
+	 */
+	private static DataSource dataSource;
+
+	/**
+	 * Used to enforce the singleton pattern.
+	 */
+	private DatabaseConnection() {
+
+	}
+
+	/**
+	 * Used to get the instance of the DatabaseConnection class.
+	 * 
+	 * @return The single instance of the DatabaseConnection class.
+	 */
+	public static synchronized DatabaseConnection getInstance() {
+		return databaseConnection;
+	}
+
+	/**
+	 * Gets the connection to the database.
+	 * 
+	 * @return the connection to the database.
+	 */
+	public Connection getConnection() {
+		return null;
+	}
+
+	/**
+	 * Used to free the connection so more users can connect at once.
+	 * 
+	 * @param c The connection to the database.
+	 */
+	public void freeConnection(Connection c) {
+
+	}
 
 }
